d5207788157d3ebd7085c615712f1517eca9b2f2
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2022-06-12 18:09:50 +0100

    add a simple cumsum function

---
+++ tests/test_core/test_cumsum.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Unit tests for cumsum function
+
+Author: G.J.J. van den Burg
+Copyright: (c) 2022, G.J.J. van den Burg
+License: See LICENSE file.
+
+"""
+
+import unittest
+
+from veld.core.cumsum import cumsum
+
+
+class CumSumTestCase(unittest.TestCase):
+    def test_cumsum_1(self):
+        x = [1, 2, 3]
+        exp = [1, 3, 6]
+        out = cumsum(x)
+        self.assertEqual(out, exp)
+
+    def test_cumsum_2(self):
+        x = []
+        exp = []
+        out = cumsum(x)
+        self.assertEqual(out, exp)
